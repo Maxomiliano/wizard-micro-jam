fe3ff0db459ca528871480aee9b5c060d0eb6346
eangeletti <angeletti.emiliano@gmail.com>
2025-03-08 19:17:58 -0300

    WandParticles

--- unity-wizard/Assets/Scripts/Gameplay/SpellsMenu.cs
+++ unity-wizard/Assets/Scripts/Gameplay/SpellsMenu.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class SpellsMenu : MonoBehaviour
 {
     [SerializeField] GameObject m_spellCanvas;
+    [SerializeField] GameObject m_wandParticles;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
             m_spellCanvas.SetActive(true);
+            m_wandParticles.SetActive(true);
         }
     }
 }
