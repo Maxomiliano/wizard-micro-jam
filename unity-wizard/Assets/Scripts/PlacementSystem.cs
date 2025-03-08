383eaa6017420ad0cf991e584659654c2ebe2baa
Maxomiliano <max.nebula1@gmail.com>
2025-03-08 17:06:47 -0300

    Tile rotation

--- unity-wizard/Assets/Scripts/PlacementSystem.cs
+++ unity-wizard/Assets/Scripts/PlacementSystem.cs
@@ -5,39 +5,40 @@ using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    //[SerializeField] private GameObject _mouseIndicator;
     [SerializeField] private GameObject _cellIndicator;
-    //[SerializeField] private InputManager _inputManager;
     [SerializeField] private Grid _grid;
     [SerializeField] private float _cellSize = 1f;
 
     private Vector3 _currentGridPosition;
+    private float _currentRotation;
 
     private void Start()
     {
-        //_currentGridPosition = _grid.WorldToCell(Vector3.zero);
         _currentGridPosition =  Vector3.zero;
         UpdateIndicatorPosition();
     }
 
     private void Update()
     {
-        /*
-        Vector3 mousePosition = _inputManager.GetSelectedMapPosition();
-        Vector3Int gridPosition = _grid.WorldToCell(mousePosition);
-        _mouseIndicator.transform.position = mousePosition;
-        _cellIndicator.transform.position = _grid.CellToWorld(gridPosition);
-        */
         if (Input.GetKeyDown(KeyCode.W)) MoveTile(new Vector3(0, 0, 1));
         if (Input.GetKeyDown(KeyCode.S)) MoveTile(new Vector3(0, 0, -1));
         if (Input.GetKeyDown(KeyCode.A)) MoveTile(new Vector3(-1, 0, 0));
         if (Input.GetKeyDown(KeyCode.D)) MoveTile(new Vector3(1, 0, 0));
+
+        if (Input.GetKeyDown(KeyCode.E)) RotateTile(45f);
+        if (Input.GetKeyDown(KeyCode.Q)) RotateTile(-45f);
     }
+
 
     public void MoveTile(Vector3 direction)
     {
         _currentGridPosition += direction * _cellSize;
         UpdateIndicatorPosition();
+    }
+    public void RotateTile(float angle)
+    {
+        _currentRotation += angle;
+        _cellIndicator.transform.localRotation = Quaternion.Euler(0, _currentRotation, 0);
     }
 
     private void UpdateIndicatorPosition()
